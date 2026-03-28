94daf61c0b310f9280ba26e5b8efd968f986b27d
unknown <23ai009@sitmng.ac.in>
2026-03-28 22:51:23 +0530

    Added counter feature

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Redux Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => dispatch(increment())}>➕</button>
+      <button onClick={() => dispatch(decrement())}>➖</button>
+    </div>
+  );
+}
+
+export default Counter;
