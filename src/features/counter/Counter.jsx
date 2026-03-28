db1712f86c37f33327e8f4d51bea79b6cf66b847
unknown <23ai009@sitmng.ac.in>
2026-03-28 22:58:59 +0530

    add the comments

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,10 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterSlice";
 
+// Counter component that displays the current count and provides buttons to increment and decrement the count
 function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
+  //the component renders the current count and two buttons to increment and decrement the count. When the buttons are clicked, they dispatch the corresponding actions to update the state in the Redux store.
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Redux Counter App</h1>
