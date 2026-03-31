bad59116fca9acbcfaff66e4b217a4292e7e13a2
unknown <23ai009@sitmng.ac.in>
2026-04-01 00:13:57 +0530

    Add the todo redux code

---
+++ src/features/todo/Todo.jsx
@@ -0,0 +1,45 @@
+import { useDispatch,useSelector } from "react-redux";
+import { addTodo, deleteTodo } from "./todoSlice.js";
+import { useState } from "react";
+
+const Todo=()=>{
+    const [input, setInput] = useState("");
+    const dispatch = useDispatch();
+    const todos = useSelector((state) => state.todo.todos);
+
+    const handleAddTodo = () => {
+        if (input.trim()) {
+            dispatch(addTodo({ id: Date.now(), text: input.trim() }));
+            setInput("");
+        }
+    };
+
+    const handleDeleteTodo = (id) => {
+        dispatch(deleteTodo(id));
+    };
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Enter a todo..."
+            />
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.text}
+                        <button onClick={() => handleDeleteTodo(todo.id)}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Todo;
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    todos:[]
+};
+
+const todoSlice=createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            state.todos.push(action.payload);
+        },
+
+        removeTodo:(state,action)=>{
+            // if false come it will throw from the list or if true it will not throw from the list
+            state.todos=state.todos.filter(todo=>todo.id!==action.payload);
+        }
+    }
+})
+
+export const {addTodo,removeTodo}=todoSlice.actions;
+export default todoSlice.reducer;
